72d83b0a7d3fee15a87f49ac9106fa3e921e9d29
Aleksandr-Shulha-dev <aleksandr.shulha.dev@gmail.com>
2022-10-06 23:21:23 +0300

    added enums and types for rtk query

---
+++ frontend/src/common/enums/index.ts
@@ -0,0 +1,10 @@
+enum API {
+  BASE = 'http://localhost:3001/api/v1',
+  GETBYID = '/get',
+  GETLIST = '/get-list',
+  CREATE = '/create',
+  UPDATE = '/update',
+  DELETE = '/delete',
+}
+
+export { API };
