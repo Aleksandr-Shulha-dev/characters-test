e672728d7c00ea04d2ef34e866f4514a9166d1e8
Aleksandr-Shulha-dev <aleksandr.shulha.dev@gmail.com>
2022-10-08 17:09:26 +0300

    created helper functions for frontend and backend

--- frontend/src/helper/index.ts
+++ frontend/src/helper/index.ts
@@ -1,3 +1,15 @@
-export const calcNumberOfPaginationList = ( count: number | undefined ): number => {
+import { FormState } from "../common/types";
+
+const calcNumberOfPaginationList = ( count: number | undefined ): number => {
   return (typeof(count) === 'undefined') ? 0: Math.ceil(count/5);
-}
+};
+
+const fillFormData = (state: FormState, data: FormData): FormData => {
+  for (const[key, val] of Object.entries(state)) {
+    data.append(key, val);
+  }
+
+  return data;
+};
+
+export { calcNumberOfPaginationList, fillFormData };
