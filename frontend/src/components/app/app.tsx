06998428c8a28f23eb130c8f906f9ca151f59947
Aleksandr-Shulha-dev <aleksandr.shulha.dev@gmail.com>
2022-10-08 17:13:23 +0300

    added new common styles, changed routing

--- frontend/src/components/app/app.tsx
+++ frontend/src/components/app/app.tsx
@@ -2,10 +2,10 @@ import { FC } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { AppRoute } from '../../common/enums';
 import { CharacterPage } from '../character-page/character-page';
-import { EditPage } from '../edit-page/edit-page';
 import { CharacterList } from '../character-list-page/character-list-page'; 
 import { Header } from '../header/header';
 import './styles.scss';
+import { UpsertPage } from '../upsert-page/upsert-page';
  
 const App: FC = () => {
   return (
@@ -15,7 +15,8 @@ const App: FC = () => {
         <Route path={AppRoute.BASE} element={<Navigate to={`${AppRoute.BASE}1`}/>}/>
         <Route path={`${AppRoute.BASE}/:id`} element={<CharacterList />}/>
         <Route path={`${AppRoute.CHARACTER}/:id`} element={<CharacterPage />}/>
-        <Route path={`${AppRoute.EDIT_CHARACTER}/:id`} element={<EditPage />}/>
+        <Route path={`${AppRoute.EDIT_CHARACTER}/:id`} element={<UpsertPage isCreatePage={false}/>}/>
+        <Route path={`${AppRoute.CREATE_CHARACTER}`} element={<UpsertPage isCreatePage={true}/>}/>
         <Route path={AppRoute.OTHER} element={<Navigate to={`${AppRoute.BASE}1`}/>}/>
       </Routes>
     </div>
